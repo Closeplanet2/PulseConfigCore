bcf5284b5654ed2625e816a6c7bf5c0b783c3739
closeplanet2 <68482902+closeplanet2@users.noreply.github.com>
2023-12-09 01:46:14 +0000

    Init Commit

---
+++ PulseConfigCore/src/main/java/com/pandapulsestudios/pulseconfigcore/PulseConfigCore.java
@@ -0,0 +1,17 @@
+package com.pandapulsestudios.pulseconfigcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PulseConfigCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
